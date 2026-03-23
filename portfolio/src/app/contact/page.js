65956e1ddd789768a40c8578429175338e73f47f
vivekpawar3010 <vivekpawar932564@gmail.com>
2026-03-23 14:06:02 +0530

    this is udpate for creating file stcurte and few line of code for initializing

---
+++ portfolio/src/app/contact/page.js
@@ -0,0 +1,1 @@
+export default function ContactPage() { return (<main style={{padding: "2rem"}}><h1>Contact</h1><p>Coming soon</p></main>); }
---
+++ portfolio/src/components/Hero.js
@@ -0,0 +1,1 @@
+export default function Hero() { return null; }
